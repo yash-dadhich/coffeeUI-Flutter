76b3104e9ec581204ade84c4e0bc2ebeff7f52f3
yash-dadhich <51815545+yash-dadhich@users.noreply.github.com>
2023-01-01 20:59:54 +0530

    basic ui completed of home page

--- coffee_ui/lib/pages/home_page.dart
+++ coffee_ui/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:coffee_ui/utils/coffee_tile.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:coffee_ui/utils/coffee_tiles.dart';
+import 'package:coffee_ui/utils/coffee_type.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,71 +13,130 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of coffee types
+  final List coffeeType = [
+    ['Cappucino', true],
+    ['Latte', false],
+    ['Black', false],
+    ['Milk', false]
+  ];
+  // user tapped on coffee types
+  void coffeeTypeSelected(int index) {
+    setState(() {
+      for (int i = 1; i < coffeeType.length; i++) {
+        coffeeType[i][1] = false;
+      }
+      coffeeType[index][1] = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu),
-        // ignore: prefer_const_literals_to_create_immutables
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20.0),
-            child: Icon(Icons.person),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        // ignore: prefer_const_literals_to_create_immutables
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: 'Favorites'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'Notifications')
-        ],
-      ),
-      body: Column(children: [
-        // Find the best coffee for you
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: Text('Find the best coffee for you',
-              style: TextStyle(
-                fontSize: 44,
-              )),
-        ),
-
-        SizedBox(height: 25),
-
-        // Search Bar
-
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25.0),
-          child: TextField(
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
-              hintText: 'Find your coffee...',
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blueGrey),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blueGrey),
-              ),
+        backgroundColor: Colors.grey[900],
+        // App bar
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: const Icon(Icons.menu),
+          actions: [
+            // ignore: prefer_const_constructors
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0),
+              child: const Icon(Icons.person),
             ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
           ),
-        ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+          ),
+        ]),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              // Text Find a coffee for you
+              Text(
+                'Find a best coffee for you',
+                style: GoogleFonts.bebasNeue(fontSize: 56),
+              ),
+              const SizedBox(height: 25),
 
-        SizedBox(height: 25),
+              // Search Bar
 
-        // Horizontal list view of coffee
-        Expanded(
-          child: ListView(scrollDirection: Axis.horizontal, children: const [
-            CoffeeTile(),
-          ]),
-        ),
-      ]),
-    );
+              TextField(
+                decoration: InputDecoration(
+                  // ignore: prefer_const_constructors
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Find your coffee...',
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade600)),
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              //
+
+              Container(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CoffeeType(
+                      coffeeType: coffeeType[index][0],
+                      isSelected: coffeeType[index][1],
+                      onTap: () {
+                        coffeeTypeSelected(index);
+                      },
+                    );
+                  },
+                ),
+              ),
+
+              // Coffee tiles
+
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    CoffeeTile(
+                      coffeeImagePath: 'lib/images/coffee2.jpg',
+                      coffeeName: 'Latte',
+                      coffeePrice: '4.20',
+                    ),
+                    CoffeeTile(
+                      coffeeImagePath: 'lib/images/coffee3.jpg',
+                      coffeeName: 'Cappucino',
+                      coffeePrice: '4.40',
+                    ),
+                    CoffeeTile(
+                      coffeeImagePath: 'lib/images/coffee4.jpg',
+                      coffeeName: 'Black',
+                      coffeePrice: '4.30',
+                    ),
+                    CoffeeTile(
+                      coffeeImagePath: 'lib/images/coffee5.jpg',
+                      coffeeName: 'Milk',
+                      coffeePrice: '4.60',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ coffee_ui/lib/utils/coffee_tiles.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class CoffeeTile extends StatelessWidget {
+  final String coffeeImagePath;
+  final String coffeeName;
+  final String coffeePrice;
+
+  CoffeeTile(
+      {required this.coffeeImagePath,
+      required this.coffeeName,
+      required this.coffeePrice});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.black54,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Coffee Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(coffeeImagePath),
+            ),
+
+            // Coffee Name
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    coffeeName,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'With Almond Milk',
+                    style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+                  ),
+                ],
+              ),
+            ),
+
+            // Price
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text('\$' + coffeePrice),
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Icon(Icons.add),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
